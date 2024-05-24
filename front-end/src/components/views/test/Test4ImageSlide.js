badaa7516c4852f3d689dc1e88a7fc50d1949ba8
yeonghongsim <a93200551@gmail.com>
2024-05-24 17:50:14 +0900

    test4에서의 이미지 슬라이드 완성

--- front-end/src/components/views/test/Test4ImageSlide.js
+++ front-end/src/components/views/test/Test4ImageSlide.js
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import styled from "styled-components"
 
 const Wrapper = styled.div`
@@ -9,93 +9,176 @@ const Wrapper = styled.div`
     justify-content: center;
 `;
 const ImageListFullContainer = styled.div`
-    width: 40rem;
-    height: 40rem;
-    box-shadow: 0 0.4rem 1.2rem 0 rgba(0, 0, 0, 0.15);
-    border-radius: 1rem;
+    width: 50rem;
+    height: 50rem;
     display: flex;
     flex-direction: column;
     align-items: flex-start;
     justify-content: flex-start;
+    border-radius: 1rem;
+    box-shadow: 0 0.4rem 1.2rem 0 rgba(0, 0, 0, 0.15);
 `;
-const IndexContainer = styled.div`
+const ImageIndexContainer = styled.div`
     width: 100%;
-    height: 3rem;
+    height: 5rem;
     display: flex;
     align-items: center;
     justify-content: flex-end;
     padding-right: 1rem;
     box-sizing: border-box;
 `;
-const IndexText = styled.p`
+const ImageIndexText = styled.p`
     font-size: 1.8rem;
     font-weight: normal;
+    color: black;
     margin: 0;
-    ${props =>
-        props.$beforeText && `
+    ${props => props.$beforeText && `
         &::before {
             content: '/';
             font-size: 1.6rem;
+            font-weight: normal;
             padding-left: 1.5rem;
             padding-right: 1.5rem;
-        }`
+        }
+        `
     }
 `;
-const ImageListContainer = styled.div`
-    width: 100%;
-    height: calc(100% - 3rem);
-`;
 const ImageListWrapper = styled.div`
+    width: 100%;
+    height: calc(100% - 5rem);
+    border-radius: 0 0 1rem 1rem;
+    overflow: hidden;
+    position: relative;
+`;
+const MoveClick = styled.div`
+    width: 5rem;
+    height: 5rem;
+    background-color: white;
+    border-radius: 50%;
+    position: absolute;
+    left: ${(props) => (props.$way === 'left' ? '1rem' : 'unset')};
+    right: ${(props) => (props.$way === 'right' ? '1rem' : 'unset')};
+    top: 50%;
+    opacity: 0.5;
+    box-shadow: 0 0.4rem 1.2rem 0 rgba(0, 0, 0, 0.25);
+    transition: all 0.75s ease;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    &:hover {
+        opacity: 1;
+        cursor: pointer;
+    }
+`;
+const MoveClickImg = styled.img`
+    width: 50%;
+    height: 50%;
+    flex-shrink: 0;
+`;
+const ImageListContainer = styled.div`
     width: 100%;
     height: 100%;
     display: flex;
     align-items: flex-start;
     justify-content: flex-start;
-    border-radius: 0 0 1rem 1rem;
-    overflow: hidden;
-    // transform: translateX(-100%);
-    // transform: translateY(-${props => props.$imageIndexCount * 100}%);
-    transition: transform 0.75s;
+    transform: translateX(-${props => props.$imageNowIndex * 100}%);
+    transition: transform 1.35s ease;
+    will-change: transform;
 `;
 const Image = styled.img`
     width: 100%;
     height: 100%;
     flex-shrink: 0;
     object-fit: cover;
-    &:hover {
-        cursor: pointer;
-    }
 `;
 
 export default function Test4ImageSlide() {
 
+    let [imageNowIndex, setImageNowIndex] = useState(0);
+    let [intervalId, setIntervalId] = useState(null);
     const imsiImageList = ['/image/imsiImage1.jpeg'
         , '/image/imsiImage2.jpeg', '/image/imsiImage3.jpeg'];
 
-    useEffect(() => (
-        console.log(1212)
-    ), []);
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setImageNowIndex((prevIndex) =>
+                prevIndex === imsiImageList.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 5000);
+
+        setIntervalId(interval);
+
+        return () => clearInterval(interval);
+    }, [
+        imsiImageList.length
+    ]);
+    const resetInterval = () => {
+        if (intervalId) {
+            clearInterval(intervalId);
+        }
+        const newInterval = setInterval(() => {
+            setImageNowIndex((prevIndex) =>
+                prevIndex === imsiImageList.length - 1 ? 0 : prevIndex + 1
+            );
+        }, 5000);
+
+        setIntervalId(newInterval);
+    };
+
+    const handleMovePrevImg = (e) => {
+        setImageNowIndex((prevIndex) =>
+            prevIndex === 0 ? imsiImageList.length - 1 : prevIndex - 1
+        );
+        e.stopPropagation();
+        resetInterval();
+    };
+    const handleMoveNextImg = (e) => {
+        setImageNowIndex((prevIndex) =>
+            prevIndex === imsiImageList.length - 1 ? 0 : prevIndex + 1
+        );
+        e.stopPropagation();
+        resetInterval();
+    };
 
     return (
         <Wrapper>
             <ImageListFullContainer>
-                <IndexContainer>
-                    <IndexText $beforeText={false}>
-                        현재 이미지 index
-                    </IndexText>
-                    <IndexText $beforeText={true}>
+                <ImageIndexContainer>
+                    <ImageIndexText $beforeText={false}>
+                        {imageNowIndex + 1}
+                    </ImageIndexText>
+                    <ImageIndexText $beforeText={true}>
                         {imsiImageList.length}
-                    </IndexText>
-                </IndexContainer>
-                <ImageListContainer>
-                    <ImageListWrapper>
+                    </ImageIndexText>
+                </ImageIndexContainer>
+                <ImageListWrapper
+                >
+                    <ImageListContainer
+                        $imageNowIndex={imageNowIndex}
+                    >
                         {
-                            imsiImageList.map((image, index) => (
-                                <Image src={image} key={index}></Image>
+                            imsiImageList.map((image, idx) => (
+                                <Image src={image} key={idx}></Image>
                             ))
                         }
-                    </ImageListWrapper>
-                </ImageListContainer>
+                    </ImageListContainer>
+                    <MoveClick
+                        $way='left'
+                    >
+                        <MoveClickImg
+                            src="/image/arrow-left.svg"
+                            onClick={handleMovePrevImg}
+                        ></MoveClickImg>
+                    </MoveClick>
+                    <MoveClick
+                        $way='right'
+                    >
+                        <MoveClickImg
+                            src="/image/arrow-right.svg"
+                            onClick={handleMoveNextImg}
+                        ></MoveClickImg>
+                    </MoveClick>
+                </ImageListWrapper>
             </ImageListFullContainer>
         </Wrapper>
     )
